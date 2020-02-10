b2707b15bf6760bedcfbce1707f0ebe79b304e02
Brandon Stull <bstull@ucf.edu>
2020-02-10 13:55:12 -0500

    Still trying to make GHPages work.

--- src/reset.js
+++ src/reset.js
@@ -1,6 +1,6 @@
 fs = require('fs')
 
 const now = new Date()
-fs.writeFile('src/time.js', `var LAST_WIN = ${now.getTime()};`, () => {
+fs.writeFile('docs/time.js', `var LAST_WIN = ${now.getTime()};`, () => {
 	console.log('You lost the game. Git commit and push.')
 })
